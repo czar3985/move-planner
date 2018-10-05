88af65619a85687a3631089cb2fabec0f9303dca
Rina Lingad <pixie.czar@gmail.com>
2018-10-06 11:57:15 +1300

    Use google map street view picture as background image

--- README.md
+++ README.md
@@ -7,3 +7,18 @@ The application takes as input a location and sends API requests to:
 
 to present a collection of data related to the location indicated.
 
+## Prerequisites
+1. Google Maps API key replaced in keys.json
+
+#### How to get a Google API key
+Refer to the guide in https://developers.google.com/maps/documentation/javascript/get-api-key
+to get an API key. You may need to indicate some billing information
+before you can proceed with acquiring an API key to be used for Google Maps API requests.
+
+-------------
+
+
+
+
+
+**_Coding ongoing_**
--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+function loadStreetView(address) {
+    // get google map api key from json file and put background image
+    $.get("keys.json", function (data) {
+        var googleMapsApiKey = data['google-api-key'];
+        var imageSource = 'https://maps.googleapis.com/maps/api/streetview?' +
+            'size=600x400&location=' + address +
+            '&key=' + googleMapsApiKey;
+
+        $('.bgimg').attr('src', imageSource);
+    });
+}
 
 function loadData() {
 
@@ -6,16 +17,17 @@ function loadData() {
     var $nytHeaderElem = $('#nytimes-header');
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
+    var address = $('#street').val() + ', ' + $('#city').val();
+
+    $greeting.text('You plan to live at ' + address + '.');
+
+    loadStreetView(address);
 
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
 
-    // load streetview
-
-    // YOUR CODE GOES HERE!
-
     return false;
-};
+}
 
 $('#form-container').submit(loadData);
